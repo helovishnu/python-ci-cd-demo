5120563ff631be03a17312a7b39694590e9e186b
Ubuntu <ubuntu@ip-10-0-3-160.ap-south-1.compute.internal>
2026-02-12 07:11:57 +0000

    Fix deploy script to use python3

--- deploy.sh
+++ deploy.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 cd /home/ubuntu/python-ci-cd-demo
 
+# Activate virtual environment
 source venv/bin/activate
 
+# Install dependencies
 pip install -r requirements.txt
 
+# Stop old app if running
 pkill -f app.py || true
 
-nohup python app.py > app.log 2>&1 &
+# Start app using python3
+nohup python3 app.py > app.log 2>&1 &
